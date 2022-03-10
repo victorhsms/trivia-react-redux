533deb4db25acd928eb8b3f0c16d09fce2fc0395
Victor Hugo <victorhsmsantos@gmail.com>
2022-03-10 18:26:59 -0300

    Requisito 5 quase completo

---
+++ src/components/Question.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Button from './Button';
+
+class Question extends Component {
+  onClickCorrectAnswer = () => {
+    console.log('correto');
+  }
+
+  onClickIncorrectAnswer = () => {
+    console.log('falso');
+  }
+
+  render() {
+    const {
+      category,
+      question,
+      type,
+      difficulty,
+      number,
+      correctAnswer,
+      incorrectAnswers,
+    } = this.props;
+    const newAnswers = [...incorrectAnswers, correctAnswer];
+    const RANGE_ALEATORIETY = 0.5;
+    const allAnswers = newAnswers.sort(() => Math.random() - RANGE_ALEATORIETY);
+    return (
+      <div>
+        <h3
+          data-testid="question-category"
+        >
+          Categoria:
+          <span>{ category }</span>
+        </h3>
+        <p>
+          Tipo:
+          { type }
+        </p>
+        <p>
+          Dificuldade:
+          { difficulty }
+        </p>
+        <h2
+          data-testid="question-text"
+        >
+          { `Questão ${number}: `}
+          <span>{ question }</span>
+        </h2>
+        <div
+          data-testid="answer-options"
+        >
+          { allAnswers.map((answer, index) => (
+            <Button
+              key={ answer }
+              textMessage={ answer }
+              id={ answer === correctAnswer
+                ? 'correct-answer' : `wrong-answer-${index === 0 ? index : index - 1}` }
+              disabled={ false }
+              onClick={ answer === correctAnswer
+                ? this.onClickCorrectAnswer
+                : this.onClickIncorrectAnswer }
+            />
+          )) }
+        </div>
+      </div>
+    );
+  }
+}
+
+Question.propTypes = {
+  category: PropTypes.string.isRequired,
+  question: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  difficulty: PropTypes.string.isRequired,
+  number: PropTypes.number.isRequired,
+  correctAnswer: PropTypes.string.isRequired,
+  incorrectAnswers: PropTypes.string.isRequired,
+};
+
+export default Question;
--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,18 +1,85 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import { tokenController } from '../actions/index';
+import getQuestions from '../services/getQuestions';
+import Question from '../components/Question';
 
 class Game extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      allQuestions: [],
+      numberQuestion: 0,
+    };
+  }
+
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(tokenController());
+    this.startGame();
+  }
+
+  showQuestion= () => {}
+
+  startGame = async () => {
+    const { token, dispatch } = this.props;
+    let questions = await getQuestions(token);
+    if (questions === []) {
+      Storage.removeItem('token');
+      dispatch(tokenController());
+      questions = await getQuestions(token);
+    }
+    console.log(questions);
+    this.setState({
+      allQuestions: questions,
+    });
+  }
+
   render() {
+    const { allQuestions } = this.state;
+    // const questionOne = allQuestions[numberQuestion];
+    // console.log(questionOne.question);
     return (
       <div>
         <Header />
+        {allQuestions.map((asks, index) => {
+          const {
+            category,
+            type,
+            difficulty,
+            question,
+            correct_answer: correctAnswer,
+            incorrect_answers: incorrectAnswers,
+          } = asks;
+
+          return (
+            <Question
+              key={ index }
+              category={ category }
+              number={ index }
+              type={ type }
+              difficulty={ difficulty }
+              question={ question }
+              correctAnswer={ correctAnswer }
+              incorrectAnswers={ incorrectAnswers }
+            />
+          );
+        })}
       </div>
     );
   }
 }
 
 Game.propTypes = {
-
+  token: PropTypes.string.isRequired,
+  dispatch: PropTypes.func.isRequired,
 };
 
-export default Game;
+const mapStateToProps = (state) => ({
+  token: state.token,
+});
+
+export default connect(mapStateToProps)(Game);
